1c68ec57f9b0efe26597f4214723c1ad389ecac1
jillian.davis96 <jilliandavis96@outlook.com>
2025-08-29 12:45:42 -0500

    Fixed debugging errors

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,22 +24,22 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp"); //fixed
 
-// What’s Wrong?
+// What’s Wrong?: Syntax issue missing quotations, closing parenthesis, and semicolon
 
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8];// fixed, changed "eight" to 8
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
-// What’s Wrong?
+// What’s Wrong?: Runtime error. The array contains a string "eight" mixed in w/ numbers. When the loop gets to "eight" it can not multiply 2 by a string and returns NaN
 
 
 
@@ -51,12 +51,12 @@ function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
 
-// What’s Wrong?
+// What’s Wrong?: Logic error, true and false are backwards. 
